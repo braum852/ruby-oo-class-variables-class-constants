3d731a59500d77aa7874b0d8801c6aec4fbae1a9
braum852 <julian.teo59@yahoo.com>
2022-02-06 21:01:30 +1000

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +12,20 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
+
+##This syntax, Book::GENRES tells us a couple of things. 
+##First, whenever we see ::, it indicates "name spacing". 
+##In other words, it tells us that the thing on the right side 
+##of the colons is a thing defined within the context, or name space, 
+##of the thing on the left side of the colons.
+
+##In this case, this is telling us that there is a constant, 
+##GENRES (we know it's a constant because it's written in all caps) 
+##that is defined within our Book class.
+
+##This is what is known as a Class Constant.
